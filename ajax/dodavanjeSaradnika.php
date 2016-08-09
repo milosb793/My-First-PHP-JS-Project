fcfb28e38f9e0c0174ffe52bb00bc4f03a770d41
Milos <milosb793@gmail.com>
2016-08-09 16:41:49 +0200

    U test fazi...

--- ajax/dodavanjeSaradnika.php
+++ ajax/dodavanjeSaradnika.php
@@ -1,30 +1,11 @@
 <?php
+require_once "../include/class.Administrator.php";
 
-$forma = "<form id='noviSaradnik' method='post' action='#' style='display: none;'>";
+//echo Administrator::dodajSaradnika($_POST['ime_prezime'],$_POST['kor_ime'],
+//                              $_POST['lozinka'],$_POST['e_mail'],$_POST['opis'],$_POST['status'],$_POST['slika_url']);
 
-$tabela = "<table border='0'>";
-$tabela .= "<th>Наслов</th>";
-$tabela .= "<tr> <td>Име и презиме: </td> <td> <input type='text' name='ime_prezime'> </td></tr>";
-$tabela .= "<tr> <td>Koр. име: </td> <td> <input type='text' name='kor_ime'> </td></tr>";
-$tabela .= "<tr> <td>Лозинка: </td> <td> <input type='password' name='lozinka1'> </td></tr>";
-$tabela .= "<tr> <td>Поновљена лозинка: </td> <td> <input type='password' name='lozinka2'> </td></tr>";
-$tabela .= "<tr> <td>Е-пошта: </td> <td> <input type='email' name='e_mail'> </td></tr>";
-$tabela .= "<tr> <td>Опис: </td> <td> <input type='text' name='opis'> </td></tr>";
+echo "Поздрав са странице!";
+function(){return "\"Поздрав са странице!\"";}
 
-$tabela .= "<tr> <td>Статус: </td> 
-<td> 
-<select id='status' name='status'>
-<option name='podrazumevano' value='---'></option> 
-<option name='1' value='aktiviran'></option> 
-<option name='0' value='deaktiviran'></option> 
-</select> </td></tr>";
 
-$tabela .= "<tr> <td>URL слике: </td> <td> <input type='url' name='slika_url'> </td></tr>";
-
-$tabela .= "</form>";
-$tabela .= "<button onclick='prosledi()'>";
-
-$forma .= $tabela;
-
-echo $forma;
-
+?>
