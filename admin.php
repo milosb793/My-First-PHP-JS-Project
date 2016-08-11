05a488d8e411b6457ce73b94f7a845753aa9ab3c
Milos <milosb793@gmail.com>
2016-08-11 19:54:03 +0200

    Sve radi dobro za sada (^_^)

--- admin.php
+++ admin.php
@@ -10,6 +10,13 @@ Metode::autorizuj("admin_id");
     <meta charset="UTF-8">
     <title>Title</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        .invalid {
+            -webkit-box-shadow: 0px 0px 14px -1px rgba(255,0,0,0.45);
+            -moz-box-shadow: 0px 0px 14px -1px rgba(255,0,0,0.45);
+            box-shadow: 0px 0px 14px -1px rgba(255,0,0,0.45);
+        }
+    </style>
 </head>
 <body>
 
@@ -20,7 +27,7 @@ Metode::autorizuj("admin_id");
 echo "Пријављен је админ са ИД-ом: {$_SESSION['admin_id']}" . "<br/>";
 echo "Кор. име: {$_SESSION['korisnik']['kor_ime']}" . "<br/>";
 echo "Лозинка(хеш): " . sha1($_SESSION['korisnik']['kor_ime']) . "<br/>";
-echo "Е-пошта: {$_SESSION['korisnik']['e_mail']}";
+echo "Е-пошта: {$_SESSION['korisnik']['e_mail']}" . '\n';
 
 // код методе, додај предмет:
 # try { Administrator::dodajPredmet() } catch($e) { echo $e->getMessag
@@ -36,22 +43,21 @@ $forma = "<form id='noviSaradnik' method='post' action='#'>";
 
 $tabela = "<table border='0'>";
 $tabela .= "<th>Наслов</th>";
-$tabela .= "<tr> <td>Име и презиме: </td> <td> <input type='text' name='ime_prezime'> </td></tr>";
-$tabela .= "<tr> <td>Koр. име: </td> <td> <input type='text' name='kor_ime'> </td></tr>";
-$tabela .= "<tr> <td>Лозинка: </td> <td> <input type='password' name='lozinka'> </td></tr>";
-$tabela .= "<tr> <td>Поновљена лозинка: </td> <td> <input type='password' name='lozinka2'> </td></tr>";
-$tabela .= "<tr> <td>Е-пошта: </td> <td> <input type='email' name='e_mail'> </td></tr>";
-$tabela .= "<tr> <td>Опис: </td> <td> <input type='text' name='opis'> </td></tr>";
-
-$tabela .= "<tr> <td>Статус: </td> 
+$tabela .= "<tr> <td>Име и презиме*: </td> <td> <input type='text' id='ime_prezime' class='reqd ime_prezime'/>      </td> <td id='greska' hidden='hidden'>" .$poruka. "</td> </tr>";
+$tabela .= "<tr> <td>Koр. име*: </td> <td> <input type='text' id='kor_ime' class='reqd kor_ime'/>               </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
+$tabela .= "<tr> <td>Лозинка*: </td> <td> <input type='password' id='lozinka' class='reqd lozinka'/>            </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
+$tabela .= "<tr> <td>Поновљена лозинка*: </td> <td> <input type='password' id='lozinka2' class='reqd lozinka'/> </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
+$tabela .= "<tr> <td>Е-пошта*: </td> <td> <input type='email' id='e_mail' class='reqd email '/>               </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
+$tabela .= "<tr> <td>Опис: </td> <td> <input type='text' id='opis' class=''/>                          </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
+$tabela .= "<tr> <td>Статус*: </td> 
 <td> 
-<select id='status' name='status'>
+<select id='status' name='status' class='' >
 <option name='podrazumevano' value='---'></option> 
 <option name='1' value='aktiviran'>Активиран</option> 
 <option name='0' value='deaktiviran'>Деактивиран</option> 
-</select> </td></tr>";
+</select> </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
 
-$tabela .= "<tr> <td>URL слике: </td> <td> <input type='url' name='slika_url'> </td></tr>";
+$tabela .= "<tr> <td>URL слике: </td> <td> <input type='url' id='slika_url' class='slika_url'/> </td> <td id='greska' hidden='hidden'>" .$poruka. "</td>  </tr>";
 
 $tabela .= "</table> </form>";
 $tabela .= "<button id='prosledi' name='prosledi'>Проследи</button>";
@@ -64,8 +70,8 @@ echo $forma;
 </div>
 
 </body>
+<!--<script type="text/javascript" src="js/polja_forme.js"></script>-->
 <script type="text/javascript" src="js/jquery-3.0.0.js"></script>
-<script type="text/javascript" src="js/polja_forme.js"></script>
 <script type="text/javascript" src="js/funkcije.js"></script>
 
 </html>
--- js/funkcije.js
+++ js/funkcije.js
@@ -1,7 +1,15 @@
-// za admin.php
+/**
+ * Created by logos on 8.8.16..
+ */
+window.onload = inicijalizujForme;
 
 
-// ДОДАВАЊЕ САРАДНИКА
+// ############### ГЛОБАЛНЕ ПРОМЕНЉИВЕ #############//
+var statusValidacje = false;
+var poruka = "";
+
+// ### ПРИКАЗ ФОРМЕ ЗА ДОДАВАЊЕ САРАДНИКА ### //
+
 
 $('#dodajSaradnikaLink').click(function() {
     $('#dodavanjeSaradnikaForma').toggle(function() {
@@ -15,39 +23,132 @@ $('#dodajSaradnikaLink').click(function() {
         }
     })
 });
+// ### КРАЈ ФУНКЦИЈЕ ПРИКАЗА ФОРМЕ ### //
 
-    // прослеђивање форме //
-$('button[name=prosledi]').click(function () {
-    var imeprez = $('input[name=ime_prezime]').val();
-    var kor_ime = $('input[name=kor_ime]').val();
-    var loz = $('input[name=lozinka]').val();
-    var em = $('input[name=e_mail]').val();
-    var op = $('input[name=opis]').val();2
+
+
+
+// ### ПРОВЕРА ФОРМИ ###
+function inicijalizujForme()
+{
+    for (var i=0; i< document.forms.length; i++)
+    {
+        document.forms[i].onsubmit = function() {return validirajFormu();}
+        //document.getElementById("prosledi").onclick = function() {statusValidacje = validirajFormu(); return statusValidacje;}
+    }
+}
+
+function validirajFormu()
+{
+    var OK = true;
+    var sviTagovi = document.getElementsByTagName("*");
+
+    for (var i=0; i<sviTagovi.length; i++)
+    {
+        if (!validirajTag(sviTagovi[i]))
+        {
+            OK = false;
+        }
+    }
+    return OK;
+
+    function validirajTag(ovajTag)
+    {
+        var spoljnaKlasa = "";
+        var sveKlase = ovajTag.className.split(" "); // Уколико имамо више речи у класи
+
+        for (var j=0; j<sveKlase.length; j++)
+        {
+            spoljnaKlasa += validiranjePoKlasi(sveKlase[j]) + " "; // овде додаје invalid ако не ваља
+        }
+
+        ovajTag.className = spoljnaKlasa;
+
+        if (spoljnaKlasa.indexOf("invalid") > -1) // ако је у класи пронађена реч Invalid
+        {
+            invalidLabel(ovajTag.parentNode);
+            ovajTag.focus();
+            if (ovajTag.nodeName == "INPUT") {
+                ovajTag.select();
+            }
+            return false;
+        }
+        return true;
+
+        function validiranjePoKlasi(ovaKlasa) {
+            var novaKlasa = "";
+
+            switch(ovaKlasa) {
+                case "":
+                case "invalid":
+                    break;
+                case "reqd":
+                    if (OK && (ovajTag.value == "" || ovajTag.value == " ") )
+                    {
+                        novaKlasa = "invalid ";
+                        poruka = "Поља означена са ознаком * не смеју бити празна.";
+                    }
+                    novaKlasa += ovaKlasa;
+                    break;
+                case "radio":
+                case "email":
+                    novaKlasa += ovaKlasa;
+                    break;
+                default:
+                    if (OK && !crossCheck(ovajTag,ovaKlasa))
+                    {
+                        novaKlasa = "invalid ";
+                        poruka = "Лозинке се не поклапају.";
+                    }
+                    novaKlasa += ovaKlasa;
+            }
+            return novaKlasa;
+        }
+
+        function crossCheck(inTag,otherFieldID) {
+            if (!document.getElementById(otherFieldID)) {
+                return false;
+            }
+            return (inTag.value == document.getElementById(otherFieldID).value);
+        }
+
+        function invalidLabel(parentTag) {
+            if (parentTag.nodeName == "TD") {
+                parentTag.className += " invalid";
+            }
+        }
+    }
+}
+
+// ### КРАЈ ПРОВЕРЕ ФОРМИ ###//
+
+// прослеђивање форме //
+$('#prosledi').click(function () {
+    var imeprez = $('#ime_prezime').val();
+    var kor_ime = $('#kor_ime').val();
+    var loz = $('#lozinka').val();
+    var em = $('#e_mail').val();
+    var op = $('#opis').val();
+
     var st = $('select[name=status] option:selected').val();
-    var url = $('input[name=slika_url]').val();
-    //proba var status;
-
-    alert("pre posta");
-    $.post("ajax/dodavanjeSaradnika.php",
-        {ime_prezime:imeprez, kor_ime:kor_ime, lozinka:loz, e_mail:em, opis:op, status:st, slika_url:url},
-        function (odgovor, status) {
-            alert("Одговор: " + odgovor +'\n'+ "Статус: " + status);
-        });
-    alert("posle posta");
+    var url = $('#slika_url').val();
 
 
-    // Не ради //
-//     $.ajax({
-//         url: "ajax/dodavanjeSaradnika.php",
-//         type: "POST",
-//         data: "ime_prezime="+imeprez + "&kor_ime=" + kor_ime + "&lozinka=" + loz + "&e_mail=" +em + "&opis=" + op + "&status=" + st + "&slika_url=" + url,
-//     success: function(odg) {
-//         alert(odg);
-//     },
-//     error: function () {
-//         alert("Greska");
-//     }
-// })
+    // $("prosledi").click( function() { statusValidacje = validirajFormu(); return statusValidacje;} );
+     statusValidacje = validirajFormu();
 
+    if(statusValidacje == true)
+    {
+        if(confirm("Јесте ли сигурни да су сви подаци у реду?"))
+        {
+            $.post("ajax/dodavanjeSaradnika.php",
+                {ime_prezime: imeprez, kor_ime: kor_ime, lozinka: loz, e_mail: em, opis: op, status: st, slika_url: url},
+                function (odgovor, status) {
+                    alert("Одговор: " + odgovor + '\n' + "Статус: " + status);
+                });
+        }
+    }
+    else
+        alert(poruka);
 });
-
+// ### КРАЈ ФУНКЦИЈЕ ЗА ДОДАВАЊЕ САРАДНИКА ### //
