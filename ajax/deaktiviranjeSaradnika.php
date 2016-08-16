021c7d6e89e140bf1a8288c20092576207ec4f62
Milos <milosb793@gmail.com>
2016-08-17 00:43:49 +0200

    Realizovano deaktiviranje saradnika

--- ajax/deaktiviranjeSaradnika.php
+++ ajax/deaktiviranjeSaradnika.php
@@ -5,9 +5,34 @@ require_once "../include/class.Administrator.php";
 require_once "../include/class.Izuzetak.php";
 require_once "../include/class.Metode.php";
 
-if(isset($_POST['sid'])) {
-
-        Administrator::deaktivirajSaradnika(intval(trim($_POST['sid'])));
-
+if(isset($_GET['zid']) && $_GET['zid'] == 2) 
+{
+    Administrator::deaktivirajSaradnika(intval(trim($_POST['sid'])));
+    return;
 }
-else echo "Нешто није у реду са пост-ом.";
+
+if(isset($_GET['zid']) && $_GET['zid'] == 1000) 
+{
+    $svi_saradnici = Saradnik::izlistajSveSaradnike();
+
+$rezultat = "<div id='deaktivirajSaradnikaDiv' style='display: none;' hidden='hidden'>
+    <br/>";
+
+$rezultat .= "<div id='padajucaLista2'>
+    Изаберите сарадника са листе: <br/>
+    <!-- Исписивање динамичке падајуће листе           -->
+    <select name='saradnici2' id='saradnici2' class='reqd' >
+        <option selected='selected' disabled='disabled'> - Изаберите сарадника - </option>";
+        while($row = $svi_saradnici->fetch_assoc())
+            $rezultat .= "<option value=' {$row['saradnik_id']}' >  {$row['ime_prezime']} </option>";
+
+   $rezultat .=  "</select>
+</div>
+<br/>
+<button id='deaktiviraj' >Деактивирај</button>
+
+</div>";
+
+    echo $rezultat;
+    return;
+}
--- js/deak_s.js
+++ js/deak_s.js
@@ -1,32 +1,40 @@
 var saradnik_id;
+
 // ### ПРИКАЗ ЛИСТЕ И ФОРМЕ ЗА ДОДАВАЊЕ САРАДНИКА ### //
 $('#deaktivirajSaradnikaLink').click(function() {
-    $('#deaktivirajSaradnikaDiv').toggle(function () {
-        if ($(this).css('display') == 'none') {
-            $(this).prop('hidden', 'hidden');
-            window.location.reload();
-        }
-        else {
-            $(this).removeProp('hidden');
-            $(this).css('display', 'block');
-        }
-    });
 
-    $("#saradnici2").change(function () {
-        saradnik_id = $(this).find("option:selected").val();
+    $.get(
+        "ajax/deaktiviranjeSaradnika.php?zid=1000",
+        {},
+        function (podaci, status) {
+            $("#sadrzaj").html(podaci);
 
-        if (saradnik_id !== undefined || saradnik_id != null || saradnik_id > 0)
-        {
-            $("#deaktiviraj").click(function () {
-                $.post("ajax/deaktiviranjeSaradnika.php",
-                    {sid: saradnik_id},
-                    function (odgovor, status) {
-                        alert(odgovor);
+            $('#deaktivirajSaradnikaDiv').toggle(function () {
+                    if ($(this).css('display') == 'none') {
+                        $(this).prop('hidden', 'hidden');
+                    }
+                    else {
+                        $(this).removeProp('hidden');
+                        $(this).css('display', 'block');
+                    }
+                }
+            );
+
+            $("#saradnici2").change(function () {
+                saradnik_id = $(this).find("option:selected").val();
+
+                if (saradnik_id !== undefined || saradnik_id != null || saradnik_id > 0) {
+                    $("#deaktiviraj").click(function () {
+                        $.post("ajax/deaktiviranjeSaradnika.php?zid=2",
+                            {sid: saradnik_id},
+                            function (odgovor, status) {
+                                alert(odgovor);
+                            });
                     });
-            });
-        }
-        else alert("ИД није у реду:" + saradnik_id);
+                }
+                else alert("ИД није у реду:" + saradnik_id);
 
-    });
+            });
+        });
 });
 
