6c2b59234991c199895cdca9f25b464877a62c65
Milos <milosb793@gmail.com>
2016-08-18 18:37:53 +0200

    Realizovano uklanjanje sa predmeta

--- ajax/uklanjanjeSaradnikPredmet.php
+++ ajax/uklanjanjeSaradnikPredmet.php
@@ -1,3 +1,63 @@
 <?php
- require_once "../include/class.Predmet.php";
+require_once "../include/class.Baza.php";
 
+require_once "../include/class.Administrator.php";
+require_once "../include/class.Saradnik.php";
+require_once "../include/class.Predmet.php";
+require_once "../include/class.Metode.php";
+
+if(isset($_GET['zid']) && $_GET['zid'] == 1000)
+{
+    $svi_saradnici = Saradnik::izlistajSveSaradnike();
+
+    $rezultat =
+        " 
+        Изаберите сарадника:
+        <select name='saradnici' id='saradnici' class='reqd'  >
+             <option selected='selected' disabled='disabled'> - Изаберите сарадника - </option> ";
+    while ($row = $svi_saradnici->fetch_assoc())
+        $rezultat .= "<option value='{$row['saradnik_id']} ' > {$row['ime_prezime']} </option>";
+
+    $rezultat .= "</select> <br/> <br/>";
+    $rezultat .= "Изаберите предмет(најпре изаберите сарадника): <br/> <div id='predmeti'></div>";
+
+
+    echo $rezultat;
+    return;
+}
+
+if(isset($_GET['zid']) && $_GET['zid'] == 1001)
+{
+    $saradnik_id = intval($_GET['saradnik_id']);
+    $predmeti_id = Predmet::vratiPredmete($saradnik_id); //враћа низ предмет ид-ова
+    $predmeti = Predmet::procitajSve(); // сви предмети
+    $niz_predmeta = array();
+    $option = "";
+
+    while($id = $predmeti_id->fetch_assoc())
+    {
+        while($predmet = $predmeti->fetch_assoc())
+        {
+            if($predmet['predmet_id'] == $id['predmet_id'])
+                $option .= "<option value='{$predmet['predmet_id']}'> {$predmet['naziv']} </option>";
+        }
+        $predmeti->data_seek(0);
+    }
+
+    $rezultat =
+        "<select name='predmeti' id='predmeti' class='reqd' >
+             <option selected='selected' disabled='disabled'> - Изаберите предмет - </option> " . $option;
+
+    $rezultat .= "</select> <br/> <br/>";
+
+    $rezultat .= "<button id='ukloni'>Уклони са предмета </button>";
+    echo $rezultat;
+    return;
+}
+if(isset($_GET['zid']) && $_GET['zid'] == 1 )
+{
+    $saradnik_id = intval($_GET['saradnik_id']);
+    $predmet_id = intval($_GET['predmet_id']);
+
+    Administrator::obrisiSaradnikaSaPredmeta($saradnik_id,$predmet_id);
+}
--- js/ukl_sar_pr.js
+++ js/ukl_sar_pr.js
@@ -1,3 +1,55 @@
-/**
- * Created by logos on 18.8.16..
- */
+var saradnik_id;
+var predmet_id;
+
+$("#ukiniAngazovanjeLink").click(function ()
+{
+    $.get(
+        "ajax/uklanjanjeSaradnikPredmet.php?zid=1000",
+        {},
+        function (podaci,status)
+        {
+            $("#sadrzaj").html(podaci); // Први део, сарадници
+
+            $("#saradnici").change(function ()
+            {
+                saradnik_id = $(this).find("option:selected").val();
+                if (saradnik_id !== undefined || saradnik_id != null || saradnik_id > 0)
+                {
+                    $.get(
+                        "ajax/uklanjanjeSaradnikPredmet.php?zid=1001&saradnik_id="+saradnik_id+"", //други део података
+                        {},
+                        function (podaci, status)
+                        {
+                            $("#predmeti").html(podaci);
+
+                            $("#predmeti").change(function ()
+                            {
+                                predmet_id = $(this).find("option:selected").val();
+
+                            });
+
+                            $("#ukloni").click(function ()
+                            {
+                                if (confirm("Јесте ли сигурни да желите да уклоните ангажовање?"))
+                                    $.get(
+                                        "ajax/uklanjanjeSaradnikPredmet.php?zid=1&saradnik_id=" + saradnik_id + "&predmet_id=" + predmet_id + "",
+                                        {},
+                                        function (odgovor, status)
+                                        {
+                                            alert(odgovor);
+                                        }
+                                    );
+
+
+                            });
+
+                        });
+                }//get zid 1001
+
+
+                 //if
+
+            });//saradnici change
+
+        }); //get 1000
+}); //click link
