cddbcc7a5e89cf92c20e7fa7b427df37eac67c10
Milos <bolic.m@mail.ru>
2016-09-01 00:07:01 +0200

    Radi lepo, samo nece da ukloni link nakon brisanja...

--- ajax/izmenaLabVezbe.php
+++ ajax/izmenaLabVezbe.php
@@ -1,45 +1,59 @@
 <?php
+session_start();
 require_once "../include/class.Korisnik.php";
+require_once "../include/class.Baza.php";
 require_once "../include/class.Metode.php";
 require_once "../include/class.Saradnik.php";
 require_once "../include/class.Predmet.php";
 require_once "../include/class.Administrator.php";
 require_once "../include/class.Materijal.php";
 require_once "../include/class.Lab_vezba.php";
+require_once "../include/class.Izuzetak.php";
 
 $svi_predmeti = Predmet::procitajSve();
 $svi_saradnici = Saradnik::izlistajSveSaradnike();
 
+// приказ изабраног предмета и листе предмета //
 if(isset($_GET['zid']) && $_GET['zid'] == 1000)
 {
+    $saradnik_id = $_SESSION['korisnik']['saradnik_id'];
+    $svi_predmeti_saradnika = Predmet::vratiPredmete($saradnik_id); // nije fetch-ovano
+
     $rezultat = "<div id='predmeti'>
-    Изаберите предмет са листе:
-    <select id='predmet' class='reqd'>
+    <select id='predmet' >
          <option value='' disabled='disabled' selected='selected'> - Изаберите предмет -</option>";
     while($predmet = $svi_predmeti->fetch_assoc() )
-        $rezultat .="<option value='{$predmet['predmet_id']}'> {$predmet['naziv']} </option>";
+    {
+        while($pr_sr = $svi_predmeti_saradnika->fetch_assoc())
+        {
+            if($predmet['predmet_id'] == $pr_sr['predmet_id'])
+                $rezultat .="<option value='{$predmet['predmet_id']}'> {$predmet['naziv']} </option>";
+        }
+        $svi_predmeti_saradnika->data_seek(0);
+    }
 
     $rezultat .= "</select> </div>
-
-    <div id='lab_vezbe' > <i style='font-size: 8pt;'> (Кад изаберете предмет, овде ће се појавити листа вежби.)</i></div>
+    <br/>
+    <div id='lab_vezbe' > <span class='poruka'> (Кад изаберете предмет, овде ће се појавити листа вежби.)</span></div>
+    <br/>
     <div id='izmenaLabVezbeForma'></div>";
 
     echo $rezultat;
     return;
 }
 
+// листа лаб. вежби //
 if(isset($_GET['zid']) && $_GET['zid'] == 1001)
 {
     $predmet_id = intval($_GET['predmet_id']);
     $sve_vezbe = Lab_vezba::procitaj_predmet_id($predmet_id); // stvarni asocijativni niz vezbi, fetch-ovan
 
     $rezultat = "
-            <select id='lab_vezba' class='reqd'>
+            <select id='lab_vezba' >
                     <option value='' disabled='disabled' selected='selected'> - Изаберите лаб. вежбу -</option>";
    foreach($sve_vezbe as $vezba)
     {
         $rezultat .= "<option value='{$vezba['lab_vezba_id']}'> {$vezba['naziv']} | {$vezba['datum_odrzavanja']} </option>";
-
     }
     $rezultat .= "</select>";
     echo $rezultat;
@@ -47,8 +61,10 @@ if(isset($_GET['zid']) && $_GET['zid'] == 1001)
 
 }
 
+// испис форме //
 if(isset($_GET['zid']) && $_GET['zid'] == 1002)
 {
+    // samo lista predmeta od saradnika, ne ubacuje materijale, testiraj
     $lab_vezba_id = intval($_GET['lab_vezba_id']);
     $vezbe = Lab_vezba::procitaj_lab_id($lab_vezba_id); //враћа вежбу
     $lab_rs = Baza::vratiInstancu()->select("SELECT * FROM laboratorija WHERE lab_vezba_id={$lab_vezba_id}");
@@ -67,56 +83,59 @@ if(isset($_GET['zid']) && $_GET['zid'] == 1002)
             break;
         }
     }
-    var_dump($ta_vezba);
+
     $predmet = Predmet::procitaj(intval($ta_vezba['predmet_id'])); //fetch-ovano
 
     //forma
     $rezultat = "
         <form id='izmenaLabVezbe' method='post' action='#' enctype='multipart/form-data'>
              <table border='0' cellpadding='' cellspacing=''>
-                  <tr> <td>Назив лаб. вежбе*: </td> <td><input type='text' id='nazivLab' class='reqd' value='{$ta_vezba['naziv']}'/>           </td> </tr>
-                  <tr> <td>Oпис лаб. вежбе*: </td> <td> <input type='text' id='opisLab' class='reqd' value='{$ta_vezba['opis']}'/>            </td> </tr>
-                  <tr> <td>Датум одржавања*: </td> <td> <input type='datetime-local' id='datumLab' class='reqd' value='{$ta_vezba['datum_odrzavanja']}'/> </td> </tr>
-                  <tr> <td>Лабораторија*: </td> <td>    <select id='brojLab' class='reqd'>
+                  <tr> <td>Назив лаб. вежбе*: </td> <td><input type='text' id='nazivLab'  value='{$ta_vezba['naziv']}'/>           </td> </tr>
+                  <tr> <td>Oпис лаб. вежбе*: </td> <td> <input type='text' id='opisLab'  value='{$ta_vezba['opis']}'/>            </td> </tr>
+                  <tr> <td>Датум одржавања*: </td> <td> <input type='text' id='datumLab'  value='{$ta_vezba['datum_odrzavanja']}'/> </td> </tr>
+                  <tr> <td>Лабораторија*: </td> <td>    <select id='brojLab' >
                         <option value='{$lab['broj_lab']}' selected='selected'> {$lab['broj_lab']} </option>";
                        foreach ($sve_lab as $lab)
                             $rezultat .= "<option value='{$lab}'> {$lab} </option>";
 
     $rezultat .=" 
-            </select>               </td> </tr>
-                <tr> <td>Предмет*: </td> <td>         <select id='predmetLab' class='reqd'>
+            </select>               </td> </tr> <br/>
+                <span class='poruka'>(Ако желите да промените сарадника, прво изаберите предмет, па сарадника).</span>
+                <tr> <td>Предмет*: </td> <td>         <select id='predmetLab' >
                         <option value='{$ta_vezba['predmet_id']}'  selected='selected'> {$predmet['naziv']}</option>";
                         while($predmet = $svi_predmeti->fetch_assoc() )
                            $rezultat .="<option value='{$predmet['predmet_id']}'> {$predmet['naziv']} </option>";
 
     $rezultat .="
             </select>            </td> </tr>
-            <tr> <td>Сарадник*: </td> <td>        <div id='sar' > <i>(изаберите најпре предмет)</i></div>";
-
+            <tr> <td>Сарадник*: </td> <td>        <div id='sar' > <span class='poruka'>(изаберите најпре предмет)</span></div>";
 
            $rezultat .= "          </td> </tr>
-            <tr> <td>Материјали: </td> <td><div id='materijali'> </div> </td> </tr> izlistavanje
-              
+            <tr> <td>Материјали: </td> <td> </td> </tr> <br/>
+            <tr><td colspan='2'> <div id='materijali'> </div> </td> </tr>
               
             <tr> <td>Додај нови материјал: </td> <td>       <input type='hidden' name='MAX_FILE_SIZE' value='200000000'> <input type='file' name='file1' id='file1' class=' '/>      </td> </tr>
         </table> <br/>
-
+        <tr> <td colspan='2'><span class='poruka'>(подржани фајлови: 'jpg','png','gif','bmp','pdf','doc','docx','txt','zip','ppt','pptx','xls','xls')</span> </td></tr> <br/>
+        <tr> <td colspan='2'><span class='poruka'>(макс. један фајл)</span> </td></tr> <br/>
+       
         <progress id='progressBar' value='0' max='100' style='width:300px; background-color: dodgerblue;'>  </progress>
     <h3 id='status'></h3>
     <p id='loaded_n_total'></p> <hr/>
-        <input type='button' id='izmeniLab' value='Измени лаб. вежбу' />
+    <button id='izmeniLab' onclick='return false;' >Измени лаб. вежбу</button>
         </form>
     ";
     echo $rezultat;
     return;
 }
 
+// приказ тренутног и свих сарадника на изабраном предмету //
 if(isset($_GET['zid']) && $_GET['zid'] == 1003)
 {
     $predmet_id = intval($_GET['predmet_id']);
     $lab_vezba_id = intval($_GET['lab_vezba_id']);
     $svi_saradnici_sa_predmeta = Saradnik::sviSaradniciNaPredmetu($predmet_id); //fetch-ovano
-    $trenutni_saradnik_rs =  Baza::vratiInstancu()->select("SELECT * FROM lab_vezba WHERE saradnik_id={$saradnik_id} AND lab_vezba_id={$lab_vezba_id}");
+    $trenutni_saradnik_rs =  Baza::vratiInstancu()->select("SELECT * FROM lab_vezba WHERE predmet_id={$predmet_id} AND lab_vezba_id={$lab_vezba_id}");
     $tr_saradnik = $trenutni_saradnik_rs->fetch_assoc();
 
     foreach ($svi_saradnici_sa_predmeta as $saradnik )
@@ -127,7 +146,7 @@ if(isset($_GET['zid']) && $_GET['zid'] == 1003)
         }
 
     $rezultat = "<select id='saradnik'>
-                    <option value='{$saradnik_id}' selected='selected' > {$tr_saradnik['ime_prezime']} </option>";
+                    <option value='{$tr_saradnik['saradnik_id']}' selected='selected' > {$tr_saradnik['ime_prezime']} </option>";
     foreach($svi_saradnici_sa_predmeta as $sar)
         $rezultat .= "<option value='{$sar['saradnik_id']}'> {$sar['ime_prezime']} </option>";
 
@@ -137,53 +156,91 @@ if(isset($_GET['zid']) && $_GET['zid'] == 1003)
     return;
 }
 
+// приказ листе материјала //
 if(isset($_GET['zid']) && $_GET['zid'] == 1004)
 {
     $lab_vezba_id = intval($_GET['lab_vezba_id']);
     $svi_materijali = Materijal::procitaj($lab_vezba_id); //fetch-ovano
     $rezultat = "";
+    $status = 0;
 
     foreach($svi_materijali as $materijal)
     {
-        $rs = Baza::vratiInstancu()->select("SELECT naziv,tip FROM fajlovi WHERE materijal_id={$materijal['materijal_id']}");
+        $rs = Baza::vratiInstancu()->select("SELECT fajl_id,naziv,tip,velicina FROM fajlovi WHERE materijal_id={$materijal['materijal_id']}");
         $fajl = $rs->fetch_assoc();
-        $tip = substr($fajl['naziv'], -4);
-        switch($tip)
+        $tip = substr($fajl['naziv'], strrpos($fajl['naziv'], '.') + 1);
+
+        switch(strtolower(trim($tip)))
         {
-            case'.pdf':
-                $rezultat .= "<img src=images/pdf.png class='ico_img'/>";
+            case'pdf':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/pdf.png class='ico_img'/>";
+                $status=1;
                 break;
-            case'.jpg':
-            case'.gif':
-            case'.jpeg':
-            case'.bmp':
-            case'.png':
-                $rezultat .= "<img src=images/image.png class='ico_img'/>";
+            case'jpg':
+            case'gif':
+            case'jpeg':
+            case'bmp':
+            case'png':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/image.png class='ico_img'/>";
+                $status=1;
                 break;
-            case'.zip':
-            case'.rar':
-                $rezultat .= "<img src=images/zip.png class='ico_img'/>";
+            case'zip':
+            case'rar':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/zip.png class='ico_img'/>";
+                $status=1;
                 break;
-            case'.txt':
-            case'.doc':
-            case'.docx':
-                $rezultat .= "<img src=images/doc.png class='ico_img'/>";
+            case'txt':
+            case'doc':
+            case'docx':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/doc.png class='ico_img'/>";
+                $status=1;
                 break;
-            case'.xls':
-            case'.xlsx':
-                $rezultat .= "<img src=images/xls.png class='ico_img'/>";
+            case'xls':
+            case'xlsx':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/xls.png class='ico_img'/>";
+                $status=1;
                 break;
-            case'.ptt':
-            case'.pttx':
-                $rezultat .= "<img src=images/ppt.png class='ico_img'/>";
+            case'ppt':
+            case'pptx':
+                $rezultat .= "<div id='fajl{$fajl['fajl_id']}' ><img src=images/ppt.png class='ico_img'/>";
+                $status=1;
                 break;
-            default: $rezultat .= "<p> нешто није у реду... Тип: {$tip} </p>";
+            default: $status=0;
                 break;
         }
-
-        $rezultat .= "<a href='ajax/download.php?materijal_id={$materijal['materijal_id']}'> {$fajl['naziv']} </a>" . "<br/>";
+        if($status==1)
+        {
+            $rezultat .= "<a  href='ajax/download.php?materijal_id={$materijal['materijal_id']}'> {$fajl['naziv']} </a>";
+            $rezultat .= "<a  class='obrisiFajl {$materijal['materijal_id']} {$fajl['fajl_id']}'   href='#' > <img src='images/recycle.png' class='ico_img' /> </a></div> <br/>";
+        }
+        $status=0;
     }
 
     echo $rezultat;
+    return;
+
+}
+
+// брисање //
+if(isset($_GET['zid']) && $_GET['zid'] == 0)
+{
+    echo "proba";
+    $fajl_id = intval($_GET['fajl_id']);
+    $materijal_id = intval($_GET['materijal_id']);
+    if( Baza::vratiInstancu()->inUpDel("DELETE FROM materijal WHERE materijal_id={$materijal_id} ") )
+        if(  Baza::vratiInstancu()->inUpDel("DELETE FROM fajlovi WHERE fajl_id={$fajl_id}") )
+        {
+            echo 'Успешно сте обрисали фајл.'; return;
+        }
+        else echo Baza::vratiInstancu()->vratiObjekatKonekcije()->error;
+    else echo Baza::vratiInstancu()->vratiObjekatKonekcije()->error;
+    return;
+}
+
+// коначно: измена вежбе
+if(isset($_GET['zid']) && $_GET['zid'] == 1)
+{
+    echo "Поздрав из zid = 1 :) lab_id: {$_POST['lab_vezba_id']} naziv: {$_POST['naziv']} \n";
+    Saradnik::izmeniLabVezbu(intval($_POST['lab_vezba_id']),$_POST['naziv'],$_POST['opis'],$_POST['datum'],intval($_POST['br_lab']),intval($_POST['predmet_id']),intval($_POST['saradnik_id']));
     return;
 }
--- js/izm_lab_v.js
+++ js/izm_lab_v.js
@@ -15,57 +15,113 @@ $("#izmeniLabVezbuLink").click(function ()
             $("#predmet").change(function ()
             {
                 predmet_id = $(this).find("option:selected").val();
-                alert(predmet_id);
+                // alert(predmet_id);
+          //change predmet
 
                 $.get(
                     "ajax/izmenaLabVezbe.php?zid=1001&predmet_id="+predmet_id+"",
                     {},
                     function (odgovor, status)
                     {
-                            $("#lab_vezbe").html(odgovor); //div
+                        $("#lab_vezbe").html(odgovor); //div lista vezbi
 
-                            $("#lab_vezba").change(function ()
+
+                        $("#lab_vezba").change(function ()
+                        {
+                            lab_vezba_id = $(this).find("option:selected").val();
+                            // alert(lab_vezba_id);
+
+                        $.get("ajax/izmenaLabVezbe.php?zid=1002&lab_vezba_id=" + lab_vezba_id + "",
+                            {},
+                            function (odgovor, status)
                             {
-                                lab_vezba_id = $(this).find("option:selected").val();
-                                alert(lab_vezba_id);
-                            });
-                            $.get("ajax/izmenaLabVezbe.php?zid=1002&lab_vezba_id="+lab_vezba_id+"",
-                                  {},
-                                function (odgovor,status)
-                                {
-                                    $("#izmenaLabVezbeForma").html(odgovor);
+                                $("#izmenaLabVezbeForma").html(odgovor);
 
-                                    $("#predmetLab").change(function ()
-                                    {
-                                        predmet_id = $(this).find("option:selected").val();
-                                        alert(predmet_id);
-                                    });
-
+                                // $("#predmetLab").change(function ()
+                                // {
+                                    predmet_id = $("#predmetLab").find("option:selected").val();
+                                    // alert(predmet_id);
+                                    // change predmet x2
                                     $.get(
-                                        "ajax/izmenaLabVezbe.php?zid=1003&predmet_id="+predmet_id+"&lab_vezba_id="+lab_vezba_id+"",
+                                        "ajax/izmenaLabVezbe.php?zid=1003&predmet_id=" + predmet_id + "&lab_vezba_id=" + lab_vezba_id + "",
                                         {},
-                                        function (odgovor,status)
+                                        function (odgovor, status)
                                         {
                                             $("#sar").html(odgovor);
-                                        }
-                                    );
 
-                                    $.get(
-                                        "ajax/izmenaLabVezbe.php?zid=1004&lab_vezba_id="+lab_vezba_id+"",
-                                        {},
-                                        function (odgovor,status)
-                                        {
-                                            $("#materijali").html(odgovor);
+                                            $.get(
+                                                "ajax/izmenaLabVezbe.php?zid=1004&lab_vezba_id=" + lab_vezba_id + "",
+                                                {},
+                                                function (odgovor, status)
+                                                {
+                                                    $("#materijali").html(odgovor);
 
-                                        }
-                                    );
+                                                    $(".obrisiFajl").click(function ()
+                                                    {
+                                                            var podaci = $(this).attr('class');
+                                                            var niz = podaci.split(" ");
+                                                            alert(podaci);
+
+                                                        if(confirm("Јесте ли сигурни да желите да обришете фајл?"))
+                                                            $.get(
+                                                                "ajax/izmenaLabVezbe.php?zid=0&materijal_id="+niz[1]+"&fajl_id="+niz[2]+"",
+                                                                {},
+                                                                function (odgovor,status)
+                                                                {
+                                                                    var fajl_id = "#fajl"+niz[2];
+                                                                    alert(odgovor);
+                                                                    if( odgovor.indexOf("Успешно") > -1 )
+                                                                        $(fajl_id).remove();
+                                                                }); //get fajl
+                                                    }); // obrisiFajl click
 
 
-                                }
-                            );
-                    }
-                );
-            });
-        }
-    );
-});
+                                                    $("#izmeniLab").click(function ()
+                                                    {
+                                                        var naziv = $("#nazivLab").val();
+                                                        var opis = $("#opisLab").val();
+                                                        var datum = $("#datumLab").val();
+                                                        var br_lab = $("#brojLab").find("option:selected").val();
+                                                        var predmet_id = $("#predmetLab").find("option:selected").val();
+                                                        var saradnik_id = $("#saradnik").find("option:selected").val();
+                                                        var fajl = $('input[type=file]')[0].files[0].name;
+                                                        var ekst = fajl.split(".").pop(); alert(ekst);
+                                                        var u_redu = true;
+                                                        var podrzani_formati = ["jpg","png","gif","bmp","pdf","doc","docx","txt","zip","ppt","pptx","xls","xls"];
+
+                                                        if(confirm("Јесте ли сигурни да су сви подаци у реду?"))
+                                                        {
+                                                           u_redu = podrzani_formati.indexOf(ekst) > -1;
+
+                                                            if(u_redu)
+                                                            {
+                                                                uploadFile(0, lab_vezba_id);
+                                                                $.post(
+                                                                    "ajax/izmenaLabVezbe.php?zid=1",
+                                                                    {
+                                                                        lab_vezba_id: lab_vezba_id,
+                                                                        naziv: naziv,
+                                                                        opis: opis,
+                                                                        datum: datum,
+                                                                        br_lab: br_lab,
+                                                                        predmet_id: predmet_id,
+                                                                        saradnik_id: saradnik_id
+                                                                    },
+                                                                    function (odgovor, status) {
+                                                                        alert("Одговор на зид = 1: " + odgovor + "статус: " + status);
+                                                                    });
+                                                            }
+                                                            else alert("Фајл није подржаног формата. ");
+                                                        }
+                                                    });// izeniLab click
+                                                }); //get 1004
+                                        }); // change lab_vezba
+                                     }); // get 1003
+                                // }); //change predmet
+                            }); // change predmet
+                        }); //get 1002
+                    }); //get 1001
+            }); //get 1000
+        }); //click link
+
+
